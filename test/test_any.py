9ee9996d21fd57f7ddec282e4aaa9682a1c71e83
Tswoen <15071340+tswoen@user.noreply.gitee.com>
2025-09-04 22:49:32 +0800

    xxxxxxxxxx

--- test/test_any.py
+++ test/test_any.py
@@ -3,7 +3,8 @@ client = OpenAI(
         api_key="sk-mvjhwoypajnggqoasejoqumfaabvifdrvztgvmxdpdyukggy",
         base_url="https://api.siliconflow.cn/v1"
 )
-embedding = client.embeddings.create(input="hello world", model="Qwen/Qwen3-Embedding-8B",dimensions=1024)
+embedding = client.embeddings.create(input=["hello world","hihihi","uio euiw e"], model="Qwen/Qwen3-Embedding-8B",dimensions=512)
 
 print(embedding.data[0].embedding)
-print(len(embedding.data[0].embedding))
+print(embedding.data[1].embedding)
+print(embedding.data[2].embedding)
